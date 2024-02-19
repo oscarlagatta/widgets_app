01143dd6a9b07a922f8daaca07d28d313ecd9b96
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-02-19 12:29:18 +0000

    Implement diverse card styles in cards screen
    Multiple card styles have been added to 'cards_screen.dart'. Six reusable card types were created, each with different properties and styles, including varying elevation levels and visual details. Furthermore, the menu link to this screen was corrected in 'menu_items.dart' to ensure navigation functions correctly.

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,7 +24,7 @@ const appMenuItems = <MenuItem>[
   MenuItem(
     title: 'Cards',
     subTitle: 'Styled Container',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card,
   )
 ];
